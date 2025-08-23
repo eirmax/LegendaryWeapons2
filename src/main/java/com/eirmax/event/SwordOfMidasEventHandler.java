f8674dbf2464c73b0c54f83881008f966bdd6072
eirmax <eirmax8@gmail.com>
2025-08-23 23:55:11 +0700

    Added handlers

--- src/main/java/com/eirmax/event/SwordOfMidasEventHandler.java
+++ src/main/java/com/eirmax/event/SwordOfMidasEventHandler.java
@@ -1,26 +1,25 @@
 package com.eirmax.event;
 
-import com.eirmax.item.custom.DragonKatana;
+import com.eirmax.item.custom.SwordOfMidas;
 import net.fabricmc.fabric.api.event.lifecycle.v1.ServerTickEvents;
 import net.minecraft.server.MinecraftServer;
 import net.minecraft.server.network.ServerPlayerEntity;
 import net.minecraft.server.world.ServerWorld;
 
-public class DragonKatanaEventHandler {
+public class SwordOfMidasEventHandler {
 
     public static void init() {
-        ServerTickEvents.END_SERVER_TICK.register(DragonKatanaEventHandler::onServerTick);
+        ServerTickEvents.END_SERVER_TICK.register(SwordOfMidasEventHandler::onServerTick);
     }
 
     public static void onServerTick(MinecraftServer server) {
         if (server.getTicks() % 20 == 0) {
             for (ServerPlayerEntity player : server.getPlayerManager().getPlayerList()) {
-                DragonKatana.applySpeedEffect(player);
+                SwordOfMidas.applyAbsorptionEffect(player);
             }
-        }
-
-        for (ServerWorld world : server.getWorlds()) {
-            DragonKatana.handleLaunchedPlayers(world);
+            for (ServerWorld world : server.getWorlds()) {
+                SwordOfMidas.handleBurningPlayers(world);
+            }
         }
     }
 }
--- src/main/java/com/eirmax/event/SwordOfWardenEventHandler.java
+++ src/main/java/com/eirmax/event/SwordOfWardenEventHandler.java
@@ -1,10 +1,10 @@
-package com.eirmax.packets;
+package com.eirmax.event;
 
 import com.eirmax.item.custom.SwordOfWarden;
 import net.fabricmc.fabric.api.event.lifecycle.v1.ServerTickEvents;
 import net.minecraft.server.network.ServerPlayerEntity;
 
-public class SwordOfWardenHandler {
+public class SwordOfWardenEventHandler {
     public static void init() {
         ServerTickEvents.END_SERVER_TICK.register(server -> {
             for (ServerPlayerEntity player : server.getPlayerManager().getPlayerList()) {
