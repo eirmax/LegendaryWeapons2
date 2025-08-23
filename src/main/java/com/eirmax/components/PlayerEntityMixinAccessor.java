9e2184e41d7fc1cdfed86963c9bbc5c0281699c6
eirmax <eirmax8@gmail.com>
2025-08-23 22:03:20 +0700

    Delete PlayerEntityMixinAccessor

--- src/main/java/com/eirmax/components/PlayerEntityMixinAccessor.java
+++
@@ -1,6 +0,0 @@
-package com.eirmax.components;
-
-public interface PlayerEntityMixinAccessor {
-        void fabric$setIgnoreFallDamage(boolean value);
-        boolean fabric$getIgnoreFallDamage();
-}
