f238b5bf7bdb8b005d38ee5b8083f08846908fd0
eirmax <eirmax8@gmail.com>
2025-08-23 21:27:08 +0700

    Update ModItems

--- src/main/java/com/eirmax/item/ModItems.java
+++ src/main/java/com/eirmax/item/ModItems.java
@@ -1,8 +1,9 @@
 package com.eirmax.item;
 
 import com.eirmax.LegendaryWeapons2;
-import com.eirmax.item.cutom.PoseidonsTrident;
-import com.eirmax.item.cutom.SwordOfWarden;
+import com.eirmax.item.custom.DragonKatana;
+import com.eirmax.item.custom.SwordOfMidas;
+import com.eirmax.item.custom.SwordOfWarden;
 import net.minecraft.item.Item;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
@@ -10,11 +11,10 @@ import net.minecraft.util.Identifier;
 
 public class ModItems {
 
-
-
-    public static final Item POSEIDONSTRIDENT = registerItem("poseidonstrident", new PoseidonsTrident(new Item.Settings().maxCount(1)));
+    public static final Item DRAGONKATANA = registerItem("dragon_katana", new DragonKatana(new Item.Settings().maxCount(1)));
     public static final Item SWORDOFWARDEN = registerItem("sword_of_warden", new SwordOfWarden(new Item.Settings().maxCount(1)));
     public static final Item ENDERBRAID = registerItem("ender_braid", new SwordOfWarden(new Item.Settings().maxCount(1)));
+    public static final Item SWORDOFMIDAS = registerItem("midas_sword", new SwordOfMidas(new Item.Settings().maxCount(1)));
 
 
 
