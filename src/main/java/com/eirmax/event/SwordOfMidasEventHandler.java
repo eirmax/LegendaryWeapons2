fb492eeb2f5bfdc600924527bae0232d71151a35
eirmax <eirmax8@gmail.com>
2025-08-23 23:57:37 +0700

    Small fix

--- src/main/java/com/eirmax/event/SwordOfMidasEventHandler.java
+++ src/main/java/com/eirmax/event/SwordOfMidasEventHandler.java
@@ -3,7 +3,6 @@ package com.eirmax.event;
 import com.eirmax.item.custom.SwordOfMidas;
 import net.fabricmc.fabric.api.event.lifecycle.v1.ServerTickEvents;
 import net.minecraft.server.MinecraftServer;
-import net.minecraft.server.network.ServerPlayerEntity;
 import net.minecraft.server.world.ServerWorld;
 
 public class SwordOfMidasEventHandler {
@@ -14,9 +13,6 @@ public class SwordOfMidasEventHandler {
 
     public static void onServerTick(MinecraftServer server) {
         if (server.getTicks() % 20 == 0) {
-            for (ServerPlayerEntity player : server.getPlayerManager().getPlayerList()) {
-                SwordOfMidas.applyAbsorptionEffect(player);
-            }
             for (ServerWorld world : server.getWorlds()) {
                 SwordOfMidas.handleBurningPlayers(world);
             }
